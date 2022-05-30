88b02bb11fbd4df0c8fabb7b80a1b0093a28c7b5
apooorvasri <38426238+apooorvasri@users.noreply.github.com>
2022-05-30 20:22:12 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,24 +6,34 @@ import FeedCard from "./components/feed/FeedCard";
 import Button from "./components/ui/Button";
 // import NavItem from "./components/navbar/NavItem";
 import Navigation from "./components/navbar/Navigation";
+import Modal from "./components/Modals/Modal";
+import React, { useState } from "react";
 
 
 const App = () =>  {
+    const [modalOpen, setModalOpen] = useState(false);
     return (
         <>
+           
+            
             {/* <NavItem></NavItem> */}
             <Navigation></Navigation>
             <Form>
                 <Input label = "Input Field" />
                 <Password />
-                <Textarea placeholder = "Description" row = "5" />
-                <Button className = "mx-auto" type = "submit">Log In</Button>
+                <Textarea placeholder = "Description" row = "2" />
+                <button className = "openModalBtn mx-auto" onClick={() => {
+                 setModalOpen(true);}} >Log In</button>
+             {modalOpen && <Modal setOpenModal={setModalOpen} />}
             </Form>
+            
             <div className = "m-4 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4">
                 <FeedCard></FeedCard>
                 <FeedCard></FeedCard>
                 <FeedCard></FeedCard>
             </div>
+           
+           
         </> 
     );
 }
