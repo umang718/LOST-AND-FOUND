cdd3f000e3e970b7686e2b15f5de7c301b46d2bb
vivekjoshi556 <vivekjoshi556@gmail.com>
2022-06-02 11:06:19 +0530

    Topics Completed: Navigation, Contact Us, Profile, Settings, Remaining: Background image for contact us page.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Navigation from "./components/layout/navbar/Navigation";
+import Content from "./components/layout/Content";
+import Footer from "./components/layout/Footer";
+
+const App = () =>  {
+    return (
+        <>
+            <Navigation></Navigation>
+            <Content></Content>
+            <Footer></Footer>
+        </>
+    );
+}
+
+export default App; 
--- src/components/ui/Card.js
+++ src/components/ui/Card.js
@@ -1,5 +1,5 @@
 const Card = (props) => {
-    let classes = "m-4 text-center rounded-lg border shadow-md p-8 border-gray-200 dark:border-gray-700 dark:text-gray-400 dark:bg-gray-800 " + props.className;
+    let classes = "m-4 rounded-lg border shadow-md p-8 border-gray-200 dark:border-gray-700 dark:text-gray-400 dark:bg-gray-800 " + props.className;
 
     return (
         <div className = { classes }>
