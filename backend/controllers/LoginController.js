d555d26b22ccb00fcbc42d8b7756845b740138f9
umang718 <umangvirat@gmail.com>
2022-06-16 23:35:36 +0530

    Reset password created successfully.

--- backend/controllers/LoginController.js
+++ backend/controllers/LoginController.js
@@ -4,6 +4,50 @@ const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, UnauthenticatedError } = require("../errors");
 const bcrypt = require("bcryptjs");
 const jwt =require('jsonwebtoken');
+const config =  require('../config/config')
+
+const nodemailer= require("nodemailer");
+const randomstring= require("randomstring");
+//const { config } = require("@fortawesome/fontawesome-svg-core");
+const { info } = require("autoprefixer");
+const bcryptjs = require("bcryptjs");
+
+const sendresetpasswordMail = async(name, email, token)=> {
+  try {
+    
+    const transporter = nodemailer.createTransport({
+      host:'smtp.gmail.com',
+      port: 25,
+      secure: true,
+      requireTLS: true,
+      auth:{
+        user:config.emailUser,
+        pass:config.emailPassword
+      }
+    });
+    const mailOptions = {
+      from:config.emailUser,
+      to: email,
+      subject:'For reset password',
+      html: '<p> Hii '+name+',Please copy the link and <a href="{{dbURL}}api/v1/auth/reset-password?token='+token+'"> reset your password </a>' 
+    }
+    console.log("hello", mailOptions);
+    transporter.sendMail(mailOptions, function(error, infor){
+      if(error)
+      {
+        console.log(error);
+      }
+      else
+      {
+        console.log("Mail has been sent to the registered mail:- ", infor.response)
+      }
+    });
+
+  } catch (error) {
+    res.status(400).send({msg:error.message})
+  }
+
+}
 
 // exports.LoginController = {
 //     // Return all the items.
@@ -12,12 +56,16 @@ const jwt =require('jsonwebtoken');
 //     }
 // }
 
-const register = async (req, res) => {
-  const user = await User.create({ ...req.body });
-  const token = jwt.sign({ userId: user._id, name:user.name }, 'jwtSecret',{
-    expiresIn:'30d', 
-  })
-  res.status(StatusCodes.CREATED).json({ user:{name: user.name },token })  
+const register = async (req, res, next) => {
+  try {
+    const user = await User.create({ ...req.body });
+    const token = jwt.sign({ userId: user._id, name:user.name }, 'jwtSecret',{
+      expiresIn:'30d', 
+    })
+    res.status(StatusCodes.CREATED).json({ user:{name: user.name },token })  
+  } catch (err) {
+    return next(err);
+  }
 };
 
 const login = async (req, res, next) => {
@@ -26,25 +74,87 @@ const login = async (req, res, next) => {
 
   if (!email || !password) {
     
-    throw new BadRequestError('Please provide email and password')
+    next(new BadRequestError('Please provide email and password')) 
+    return;
 
   }
 
   const user = await User.findOne({ email })
   //console.log(user);
   if (!user) {
-    throw new UnauthenticatedError('Invalid Credentials')
+    next(new UnauthenticatedError('Invalid Credentials')) 
+    return;
   }
   const isPasswordCorrect = await user.comparePassword(password)
   if (!isPasswordCorrect) {
-    throw new UnauthenticatedError('Invalid Credentials')
+    next(new UnauthenticatedError('Invalid Credentials')) 
+    return;
   }
+
   // compare password
   const token = user.createJWT()
   res.status(StatusCodes.OK).json({ user: { name: user.name }, token })
 };
 
+const forgetPassword = async(req,res) => {
+
+  try {
+    const email = req.body.email;
+    const userData = await User.findOne({ email:email });
+    if(userData){
+        const randomString = randomstring.generate();
+        const data = await User.updateOne({email:email}, {$set:{token: randomString}})
+        sendresetpasswordMail(userData.name, userData.email,randomString);
+        res.status(200).send({msg:"Please check you inbox and reset your password."});
+    }
+    else{
+      res.status(200).send({msg:"This email does not exists."});
+    }
+    
+  } catch (error) {
+      res.status(400).send({msg:error.message});
+  }
+   
+}
+
+const securePassword = async (password)=>{
+  try {
+    const passwordHash = await bcryptjs.hash(password,10);
+    return passwordHash;
+  } catch (error) {
+    res.status(400).send({msg:error.message});
+  }
+}
+
+
+const resetpassword = async (req, res) =>{
+  try {
+    
+    const token = req.query.token;
+    const tokenData = await User.findOne({ token: token })
+    if(tokenData)
+    {
+        const password = req.body.password;
+        const newPassword = await securePassword(password);
+        const userData = await User.findByIdAndUpdate({ _id: tokenData._id  },{$set:{password:newPassword, token:'' }},{new:true});
+        res.status(200).send({msg:"User password has been reset successfully", data: userData});
+    }
+    else{
+      res.status(200).send({msg:"this link has been expired."});
+    }
+  } catch (error) {
+    res.status(400).send({msg:error.message});
+    
+  }
+
+}
+
+
 module.exports = {
   register,
   login,
+  forgetPassword,
+  resetpassword,
+  securePassword,
+  sendresetpasswordMail
 };
--- backend/middleware/error-handler.js
+++ backend/middleware/error-handler.js
@@ -1,4 +1,3 @@
-
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
   let customError = {
@@ -17,18 +16,18 @@ const errorHandlerMiddleware = (err, req, res, next) => {
       .join(',')
     customError.statusCode = 400
   }
+
   if (err.code && err.code === 11000) {
     customError.msg = `Duplicate value entered for ${Object.keys(
       err.keyValue
     )} field, please choose another value`
     customError.statusCode = 400
   }
+
   if (err.name === 'CastError') {
     customError.msg = `No item found with id : ${err.value}`
     customError.statusCode = 404
   }
-
-  
 
   return res.status(customError.statusCode).json({ msg: customError.msg })
 }
